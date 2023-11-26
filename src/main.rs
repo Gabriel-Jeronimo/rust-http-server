ac8e1446ca39cb116e1ead12d76f87e186fb59ae
Gabriel Jeronimo <gabrieljeronimo-@hotmail.com>
2023-11-25 21:01:00 -0300

    Add format to request and respond with the route requested

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    io::Read,
+    io::{Result, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn handle(stream: TcpStream) {
-    // TODO: read stream data
+struct Request {
+    httpVerb: String,
+    route: String,
 }
 
-fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+fn handle(mut stream: TcpStream) -> Result<()> {
+    let data = &mut [0; 128];
+    stream.read(data)?;
 
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let request = httpRequestToObject(&String::from_utf8_lossy(data))?;
 
+    // println!("{}", request.route);
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\n\r\nRequested path: {}\r\n",
+        request.route
+    );
+
+    stream.write(response.as_bytes());
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    const PORT: &str = "8080";
+
+    let listener = TcpListener::bind(format!("{}:{}", "localhost", PORT))?;
+
+    println!("app listening on port {}", PORT);
     for stream in listener.incoming() {
         handle(stream?);
     }
 
     Ok(())
+}
+
+fn httpRequestToObject(request: &str) -> Result<Request> {
+    let splited: Vec<&str> = request.split(" ").collect();
+
+    Ok(Request {
+        httpVerb: splited[0].to_string(),
+        route: splited[1].to_string(),
+    })
 }
